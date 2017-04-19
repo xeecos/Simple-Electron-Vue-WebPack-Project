61ae1f46fbe229ab2a092eb37f4ce58d8bbf2b52
xeecos <flashindream@gmail.com>
2017-04-19 16:10:45 +0800

    update

--- src/renderer/layouts/main.vue
+++ src/renderer/layouts/main.vue
@@ -16,11 +16,3 @@
 <style>
     @import '../styles/style.css';
 </style>
-<style scoped>
-  .container {
-    max-width: 600px;
-    margin: 0 auto;
-    padding: 15px 30px;
-    background: #f9f7f5;
-  }
-</style>
